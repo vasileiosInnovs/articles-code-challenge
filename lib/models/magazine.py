d4740a50aeee6205d3b994ce15bdb7f578267104
Hagee <hagee.ktr@outlook.com>
2025-05-26 20:42:41 +0300

    [Feature]: Add code that creates tables and drops tables

--- lib/models/magazine.py
+++ lib/models/magazine.py
@@ -21,3 +21,30 @@ class Magazine:
         if not isinstance(name, str):
             raise ValueError("Name of magazine has to be a string")
         self._name = name
+
+    @classmethod
+    def create_class(cls):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        sql = """
+            CREATE TABLE IF NOT EXISTS magazines (
+            id INTEGER PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            category VARCHAR(255) NOT NULL
+            )
+        """
+        cursor.execute(sql)
+        conn.commit()
+
+    @classmethod
+    def drop_class(cls):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        sql = """
+            DROP TABLE IF EXISTS magazines;
+        """
+
+        cursor.execute(sql)
+        conn.commit()
