d369e4a381eb916c5c4638a13cd944abf97a77cd
Hagee <hagee.ktr@outlook.com>
2025-05-26 22:34:07 +0300

    [Feature]: Generate class methods to find magazine by category and name

--- lib/models/magazine.py
+++ lib/models/magazine.py
@@ -4,10 +4,10 @@ class Magazine:
 
     all = {}
 
-    def __init__(self, name, category):
+    def __init__(self, name, category, id=None):
+        self.id = id
         self.name = name
         self.category = category
-        Magazine.all.update()
 
     def __repr__(self):
         return f'<Magazine {self.name}: Category {self.category}>'
@@ -29,9 +29,9 @@ class Magazine:
 
         sql = """
             CREATE TABLE IF NOT EXISTS magazines (
-            id INTEGER PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            category VARCHAR(255) NOT NULL
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                category TEXT NOT NULL
             )
         """
         cursor.execute(sql)
@@ -42,10 +42,7 @@ class Magazine:
         conn = get_connection()
         cursor = conn.cursor()
 
-        sql = """
-            DROP TABLE IF EXISTS magazines;
-        """
-
+        sql = "DROP TABLE IF EXISTS magazines;"
         cursor.execute(sql)
         conn.commit()
 
@@ -54,10 +51,9 @@ class Magazine:
         cursor = conn.cursor()
 
         sql = """
-            INSERT INTO magazines (self.name, self.category)
+            INSERT INTO magazines (name, category)
             VALUES (?, ?)
         """
-
         cursor.execute(sql, (self.name, self.category))
         conn.commit()
 
@@ -65,7 +61,45 @@ class Magazine:
         type(self).all[self.id] = self
 
     @classmethod
-    def create(cls, name):
-        magazine = cls(name)
+    def create(cls, name, category):
+        magazine = cls(name, category)
         magazine.save()
         return magazine
+
+    @classmethod
+    def instance_from_db(cls, row):
+        magazine = cls.all.get(row[0])
+        if magazine:
+            magazine.name = row[1]
+            magazine.category = row[2]
+        else:
+            magazine = cls(row[1], row[2], id=row[0])
+            cls.all[magazine.id] = magazine
+        return magazine
+    
+    @classmethod
+    def find_by_id(cls, id):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        sql = "SELECT * FROM magazines WHERE id = ?"
+        row = cursor.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+    
+    @classmethod
+    def find_by_category(cls, category):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        sql = "SELECT * FROM magazines WHERE category = ?"
+        row = cursor.execute(sql, (category,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+    
+    @classmethod
+    def find_by_name(cls, name):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        sql = "SELECT * FROM magazines WHERE name = ?"
+        row = cursor.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
